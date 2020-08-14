18a2ae0c083738ffc880a7339c8780cacf1d04af
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-14 11:33:58 +0430

    video 10 - Scope definition. @Singleton annotation usage when we can annotate a component like AppComponent with it and again annotate dependencies in corresponding modules of that component.

--- app/src/main/java/com/test/daggermitch/di/AppComponent.java
+++ app/src/main/java/com/test/daggermitch/di/AppComponent.java
@@ -4,12 +4,15 @@ import android.app.Application;
 
 import com.test.daggermitch.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(modules =
         {AndroidSupportInjectionModule.class, ActivityBuildersModule.class,AppModule.class})
 public interface AppComponent extends AndroidInjector<BaseApplication> {
--- app/src/main/java/com/test/daggermitch/di/AppModule.java
+++ app/src/main/java/com/test/daggermitch/di/AppModule.java
@@ -10,22 +10,27 @@ import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.test.daggermitch.R;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public abstract class AppModule {
 
+    @Singleton
     @Provides
     public static RequestOptions provideRequestOption() {
         return RequestOptions.placeholderOf(R.drawable.white_background).error(R.drawable.white_background);
     }
 
+    @Singleton
     @Provides
     public static RequestManager glideInstance(Application application, RequestOptions requestOptions) {
         return Glide.with(application).setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     public static Drawable getAppDrawable(Application application) {
         return ContextCompat.getDrawable(application, R.drawable.logo);
