0ca4f425a1a43a5ed6ee37d0ce237022e0e6417b
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-14 09:01:18 +0430

    video 9 - injecting glide and app drawable instance to auth activity

--- app/src/main/java/com/test/daggermitch/AuthActivity.java
+++ app/src/main/java/com/test/daggermitch/AuthActivity.java
@@ -1,7 +1,10 @@
 package com.test.daggermitch;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
 
 import javax.inject.Inject;
 
@@ -11,15 +14,14 @@ public class AuthActivity extends DaggerAppCompatActivity {
 
     private static final String TAG = "AuthActivity";
     @Inject
-    public String mFuckerString;
+    public Drawable logo;
     @Inject
-    public boolean isAppNull;
+    public RequestManager glideInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        Log.d(TAG, "onCreate: "+mFuckerString);
-        Log.d(TAG, "onCreate: is app null? "+isAppNull);
+        glideInstance.load(logo).into((ImageView) findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/test/daggermitch/di/AppModule.java
+++ app/src/main/java/com/test/daggermitch/di/AppModule.java
@@ -1,6 +1,14 @@
 package com.test.daggermitch.di;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.test.daggermitch.R;
 
 import dagger.Module;
 import dagger.Provides;
@@ -9,12 +17,17 @@ import dagger.Provides;
 public abstract class AppModule {
 
     @Provides
-    public static String getFuckerString(){
-        return "Trump is MotherFucker";
+    public static RequestOptions provideRequestOption() {
+        return RequestOptions.placeholderOf(R.drawable.white_background).error(R.drawable.white_background);
     }
 
     @Provides
-    public static boolean getApp(Application application){
-        return application == null;
+    public static RequestManager glideInstance(Application application, RequestOptions requestOptions) {
+        return Glide.with(application).setDefaultRequestOptions(requestOptions);
+    }
+
+    @Provides
+    public static Drawable getAppDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 }
