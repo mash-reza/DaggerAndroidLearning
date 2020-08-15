107f8894ce0e598b53a828653c32a125220b975f
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-15 19:14:13 +0430

    video 26 - show user detail in profile fragment

--- app/src/main/java/com/test/daggermitch/ui/main/ProfileFragment.java
+++ app/src/main/java/com/test/daggermitch/ui/main/ProfileFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.test.daggermitch.R;
-import com.test.daggermitch.di.ViewModelsFactoryModule;
+import com.test.daggermitch.model.User;
 import com.test.daggermitch.viewmodel.ViewModelProvidersFactory;
 
 import javax.inject.Inject;
@@ -21,6 +23,10 @@ public class ProfileFragment extends DaggerFragment {
     private ViewModelProvidersFactory providersFactory;
     private ProfileViewModel profileViewModel;
 
+    private TextView email;
+    private TextView username;
+    private TextView website;
+
     @Inject
     public ProfileFragment(ViewModelProvidersFactory viewModelProvidersFactory) {
         this.providersFactory = viewModelProvidersFactory;
@@ -29,10 +35,42 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Toast.makeText(requireContext(), "Profile Fragment", Toast.LENGTH_SHORT).show();
 
-        profileViewModel = new ViewModelProvider(this,providersFactory).get(ProfileViewModel.class);
+        profileViewModel = new ViewModelProvider(this, providersFactory).get(ProfileViewModel.class);
 
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+
+        profileViewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        profileViewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), userAuthResource -> {
+            if (userAuthResource != null) {
+                switch (userAuthResource.status) {
+                    case AUTHENTICATED: {
+                        showUserDetails(userAuthResource.data);
+                        break;
+                    }
+                    case ERROR: {
+                        showErrorDetails(userAuthResource.message);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    private void showErrorDetails(String message) {
+        email.setText(message);
+    }
+
+    private void showUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
     }
 }
--- app/src/main/java/com/test/daggermitch/ui/main/ProfileViewModel.java
+++ app/src/main/java/com/test/daggermitch/ui/main/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.test.daggermitch.ui.main;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.test.daggermitch.SessionManager;
+import com.test.daggermitch.model.User;
+import com.test.daggermitch.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,15 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private LiveData<AuthResource<User>> authenticatedUser;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
         Log.d(TAG, "ProfileViewModel: profile view model is working... shit yes");
+        authenticatedUser = sessionManager.getAuthUser();
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return authenticatedUser;
     }
 }
