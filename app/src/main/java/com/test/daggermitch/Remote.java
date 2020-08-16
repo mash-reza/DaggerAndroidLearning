1256e598fca92d63debf1da92b66f31913523c91
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-16 20:17:14 +0430

    Method Injection

---
+++ app/src/main/java/com/test/daggermitch/Remote.java
@@ -0,0 +1,18 @@
+package com.test.daggermitch;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Remote {
+
+    private static final String TAG = "Remote";
+
+    @Inject
+    public Remote() {
+    }
+
+    public void setListener(Car car) {
+        Log.i(TAG, "Remote Connected.");
+    }
+}
